dfa5b9423e6385ffc4f34d14805f977371df7758
MitinSany <mitinsoft@gmail.com>
2017-02-22 02:15:00 +0200

    first beta

--- src/RrdCachedClient.php
+++ src/RrdCachedClient.php
@@ -4,12 +4,14 @@ namespace RrdCached;
 
 use Socket\Raw\Socket;
 use Socket\Raw\Factory;
-use RrdCached\RrdCachedException;
 
 class RrdCachedClient
 {
     /** @var bool */
-    protected $batchEnabled = false;
+    protected $autoParse = true;
+
+    /** @var bool */
+    protected $batchMode = false;
 
     /** @var array */
     protected $batchCommands = [];
@@ -19,6 +21,12 @@ class RrdCachedClient
 
     /** @var Socket */
     protected $socket;
+
+    /** @var array */
+    public $defaultCreateParams = [];
+
+    /** @var int */
+    public $defaultCreateStep = 300;
 
     function __construct($socketPath = 'unix:///var/run/rrdcached.sock')
     {
@@ -40,88 +48,257 @@ class RrdCachedClient
     function write($command)
     {
         $this->socket->write($command);
-        return $this->readAndParse();
     }
 
-    protected function readAndParse()
+    protected function parseLn(string $line): int
+    {
+        $parts = explode(' ', $line);
+        return (int)$parts[0];
+    }
+
+    protected function readAndParse(): string
     {
         $readBuffLen = 2048;
         $serverMessage = $this->socket->read($readBuffLen, PHP_NORMAL_READ);
-        $parts = explode(' ', $serverMessage);
+        $returnCode = $this->parseLn($serverMessage);
 
         $result = '';
 
-        for ($i = 0; $i < $parts[0] - 1; $i++) {
+        for ($i = 1; $i <= $returnCode; $i++) {
             $result .= $this->socket->read($readBuffLen, PHP_NORMAL_READ);
         }
 
-        return $result;
+        return '' !== $result ? $result : $serverMessage;
     }
 
-    function readAll()
+    function help($command): string
     {
-        $readBytes = 16;
-        $result = '';
-        do {
-            $buff = $this->socket->read($readBytes);
-            $result .= $buff;
-            $len = strlen($buff);
-        } while ($readBytes === $len);
-
-        return $result;
-    }
-
-    function help($param)
-    {
-        return $this->writeCommand('HELP', $param);
-    }
-
-    protected function writeCommand($command, $param = '')
-    {
-        if ($this->batchEnabled) {
-            $this->batchCommands[] = [
-                'command' => $command,
-                'param' => $param
-            ];
-
-            return true;
-        } else {
-            return $this->write(sprintf('%s %s' . PHP_EOL, $command, $param));
-        }
-
+        $this->write('HELP ' . $command . PHP_EOL);
+        return $this->readAndParse();
     }
 
     function stats()
     {
-        return $this->writeCommand('STATS');
+        $this->write('STATS' . PHP_EOL);
+        return $this->readAndParse();
     }
 
     function quit()
     {
-        return $this->writeCommand('QUIT');
+        $this->write('QUIT' . PHP_EOL);
     }
 
-    function update($fileName, $timestamp, $data)
+    function update($fileName, array $options)
     {
-        return $this->writeCommand('UPDATE', $fileName . ' ' . $timestamp . ':' . $data);
+        if ($this->batchMode) {
+            $this->batchCommands[] = [
+                'command' => 'update',
+                'fileName' => $fileName,
+                'options' => $options
+            ];
+        } else {
+            $this->write('UPDATE ' . $fileName . ' ' . implode(':', $options) . PHP_EOL);
+
+            if ($this->autoParse) {
+                return $this->readAndParse();
+            }
+        }
+    }
+
+    protected function updateErrorHandler(string $fileName, array $options)
+    {
+        $this->autoParse = false;
+
+        $this->create($fileName, $this->defaultCreateParams);
+        $this->update($fileName, $options);
+    }
+
+    function create(string $fileName, array $options)
+    {
+        if (0 < count($options)) {
+            $workOptions = $options;
+        } elseif (0 < count($this->defaultCreateParams)) {
+            $workOptions = $this->defaultCreateParams;
+        } else {
+            throw new RrdCachedException('Missing options for create RRD file. Set array of options to parameter
+            defaultCreateParams or send as parameter in create function');
+        }
+
+
+        if ($this->batchMode) {
+            $this->batchCommands[] = [
+                'command' => 'create',
+                'fileName' => $fileName,
+                'options' => $workOptions
+            ];
+        } else {
+            $this->write('CREATE ' . $fileName . ' ' . implode(' ', $workOptions) . PHP_EOL);
+
+            if ($this->autoParse) {
+                return $this->readAndParse();
+            }
+        }
     }
 
     function batchBegin()
     {
-        $this->batchEnabled = true;
+        $this->write('BATCH' . PHP_EOL);
+        $this->batchMode = true;
+        $this->readAndParse();
     }
 
     function batchCommit()
     {
-        $this->batchEnabled = false;
-
-        $this->writeCommand('BATCH');
+        $this->batchMode = false;
+        $this->autoParse = false;
 
         foreach ($this->batchCommands as $k => $item) {
-            $this->writeCommand($item['command'], $item['param']);
+
+            switch ($item['command']) {
+
+                case 'create':
+                    $this->create($item['fileName'], $item['options']);
+                    break;
+
+                case 'update';
+                    $this->update($item['fileName'], $item['options']);
+                    break;
+
+                case 'flush';
+                    $this->flush($item['fileName']);
+                    break;
+
+                case 'forget';
+                    $this->forget($item['fileName']);
+                    break;
+
+                case 'wrote';
+                    $this->wrote($item['fileName']);
+                    break;
+            }
         }
 
-        return $this->writeCommand('.');
+        $this->write(".\n");
+
+        $batchResultRaw = $this->readAndParse();
+
+        $batchResult = explode("\n", rtrim($batchResultRaw));
+
+        foreach ($batchResult as $k => $v) {
+            $returnCommandNum = $this->parseLn($v) - 1;
+            $batchItem = $this->batchCommands[$returnCommandNum];
+
+            switch ($batchItem['command']) {
+                case 'update';
+                    $this->updateErrorHandler($batchItem['fileName'], $batchItem['options']);
+                    break;
+
+                case 'create':
+                    if (0 < strpos($v, 'File exists')) {
+                        continue;
+                    }
+                    throw new RrdCachedException('Error create file ' . $batchItem['fileName']);
+
+            }
+        }
+
+        $this->autoParse = true;
+    }
+
+    function flush(string $fileName)
+    {
+        if ($this->batchMode) {
+            $this->batchCommands[] = [
+                'command' => 'flush',
+                'fileName' => $fileName
+            ];
+        } else {
+            $this->write("flush $fileName\n");
+
+            if ($this->autoParse) {
+                return $this->readAndParse();
+            }
+        }
+    }
+
+    function wrote(string $fileName)
+    {
+        if ($this->batchMode) {
+            $this->batchCommands[] = [
+                'command' => 'wrote',
+                'fileName' => $fileName
+            ];
+        } else {
+            $this->write("wrote $fileName\n");
+
+            if ($this->autoParse) {
+                return $this->readAndParse();
+            }
+        }
+    }
+
+    function flushAll()
+    {
+        $this->write("FLUSHALL\n");
+        return $this->readAndParse();
+    }
+
+    function pending(string $fileName)
+    {
+        $this->write("PENDING $fileName\n");
+        return $this->readAndParse();
+    }
+
+    function forget(string $fileName)
+    {
+        if ($this->batchMode) {
+            $this->batchCommands[] = [
+                'command' => 'forget',
+                'fileName' => $fileName
+            ];
+        } else {
+            $this->write("FORGET $fileName\n");
+
+            if ($this->autoParse) {
+                return $this->readAndParse();
+            }
+        }
+    }
+
+    function queue()
+    {
+        $this->write("QUEUE\n");
+        return $this->readAndParse();
+    }
+
+    function fetch(string $fileName, array $options)
+    {
+        $this->write("FETCH $fileName " . implode(' ', $options) . "\n");
+        return $this->readAndParse();
+    }
+
+    function fetchBin(string $fileName, array $options)
+    {
+        $this->write("FETCHBIN $fileName " . implode(' ', $options) . "\n");
+        return $this->readAndParse();
+    }
+
+    function info(string $fileName)
+    {
+        $this->write("INFO $fileName\n");
+        return $this->readAndParse();
+    }
+
+    function first(string $fileName, int $raaIndex = 0)
+    {
+        $this->write("FIRST $fileName $raaIndex\n");
+        return $this->readAndParse();
+    }
+
+    function last(string $fileName)
+    {
+        $this->write("LAST $fileName\n");
+        return $this->readAndParse();
     }
 
 }
